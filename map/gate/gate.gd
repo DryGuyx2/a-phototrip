d01e5ea970aeb3273e6ef25e4e9879f0c2f96b18
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-02-18 17:50:44 +0100

    Exit system change

--- map/gate/gate.gd
+++ map/gate/gate.gd
@@ -1,11 +1,11 @@
 extends Area2D
-class_name Exit
+class_name Gate
 
 @export var detection_mask: int
-@export var entrance: Node2D
-@export var entrance_camera_position: Node2D
+@export var exit_point: Node2D
+@export var camera_position: Node2D
 
-var camera: Camera2D
+@export var camera: Camera2D
 
 # REMEMBER: Connect on exit body entered signal to itself
 
@@ -13,5 +13,5 @@ func _ready() -> void:
 	set_collision_mask_value(detection_mask, true)
 
 func _on_body_entered(body) -> void:
-	body.global_position = entrance.position
-	camera.global_position = entrance_camera_position.position
+	body.global_position = exit_point.position
+	camera.global_position = camera_position.position
