f340bdec2a6a9c64222d2f128ebb15a63b51021e
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-02-18 01:08:08 +0100

    Refactor player, add html export, add photographing

--- player/Player.gd
+++ player/Player.gd
@@ -2,23 +2,48 @@ extends CharacterBody2D
 
 @export var speed: int = 1000
 
-var animation_component: AnimatedSprite2D
+@onready var animation_component: AnimatedSprite2D = $AnimatedSprite2D
 
-func _ready():
-	animation_component = $AnimatedSprite2D
+var equipped_camera: bool = false
+var photographing: bool = false
+var direction: Vector2 = Vector2.ZERO
 
 func _process(delta: float):
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if input_direction.x == 1:
+	handle_input()
+	handle_animations()
+	
+	if not photographing:
+		position += direction * speed * delta
+	move_and_slide()
+
+func handle_input():
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if Input.is_action_just_pressed("equip"):
+		equipped_camera = not equipped_camera
+	
+	if Input.is_action_just_pressed("interact"):
+		photograph()
+
+func handle_animations():
+	if direction.x == 1:
 		animation_component.flip_h = false
-	elif input_direction.x == -1:
+	elif direction.x == -1:
 		animation_component.flip_h = true
 	
-	if input_direction.x == 0 and input_direction.y == 0:
+	if direction.x == 0 and direction.y == 0:
+		if equipped_camera:
+			animation_component.play("idle_camera")
+			return
 		animation_component.play("idle")
 	else:
+		if equipped_camera:
+			animation_component.play("moving_camera")
+			return
 		animation_component.play("moving")
-	
-	
-	position += input_direction * speed * delta
-	move_and_slide()
+
+func photograph():
+	if equipped_camera:
+		photographing = true
+		print("Photographing...")
+		photographing = false
