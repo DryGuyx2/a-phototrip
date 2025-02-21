8cb52bd06df32314a13d685f4aaf774a9c64b936
DryGuyx2 <130485254+DryGuyx2@users.noreply.github.com>
2025-02-21 15:22:14 +0100

    Fix cult trigger bug, fix gate shape

--- objects/cult/cult_scene_trigger.gd
+++ objects/cult/cult_scene_trigger.gd
@@ -3,12 +3,14 @@ extends Area2D
 signal triggered_scene
 
 var player_slept = false
+var triggered = false
 
 func _ready() -> void:
 	set_collision_mask_value(GlobalData.layers["scene_trigger"], true)
 
 func _on_body_entered(body):
-	if player_slept:
+	if player_slept and not triggered:
+		triggered = true
 		emit_signal("triggered_scene")
 
 func _on_camp_sleep():
